056013ac57de0e709880ed021eee380fe51fa649
Tiutmattiu <killordeal@outlook.com>
2026-08-31 20:44:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const ASL_CONFIG = {
   WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzae_Opq594EknAFuvYK9enBRWe9fBlOAtAeP2zhuOYe7Tci5mLV1YONXqh2iDfFP_88A/exec",
-  POLYU_CONTACT: "To be provided",
-  TMH_CONTACT: "To be provided"
+  POLYU_CONTACT: "+852 91230084 排小姐",
+  TMH_CONTACT: "+852 24686962 肖博士"
 };
