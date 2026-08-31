17482af21b54e1822772e163895d01ddc77b8ac6
Tiutmattiu <killordeal@outlook.com>
2026-08-31 19:50:52 +0800

    Connect frontend to Apps Script deployment

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const ASL_CONFIG = {
-  WEB_APP_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzae_Opq594EknAFuvYK9enBRWe9fBlOAtAeP2zhuOYe7Tci5mLV1YONXqh2iDfFP_88A/exec",
   POLYU_CONTACT: "To be provided",
   TMH_CONTACT: "To be provided"
 };
