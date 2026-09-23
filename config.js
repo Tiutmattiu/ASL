0e9b8af504c3af2bc8e508c43fa817c65de67316
Tiutmattiu <killordeal@outlook.com>
2026-09-23 12:57:35 +0800

    Use study contact for participant portal

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const ASL_CONFIG = {
   WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzae_Opq594EknAFuvYK9enBRWe9fBlOAtAeP2zhuOYe7Tci5mLV1YONXqh2iDfFP_88A/exec",
-  POLYU_CONTACT: "+852 91230084 排小姐",
-  TMH_CONTACT: "+852 24686962 肖博士"
+  CONTACT_PHONE: "+85291230084",
+  WHATSAPP_URL: "https://wa.me/85291230084"
 };
